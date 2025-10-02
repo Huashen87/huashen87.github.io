dd64156be5c6fd122667f2dd7dcb5195932c752f
Huashen87 <HuashenCoding@gmail.com>
2025-10-02 23:58:51 +0800

    add new post

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -6,6 +6,11 @@ export interface Post {
 
 const posts: Post[] = [
   {
+    title: 'One Month, One Game',
+    tags: ['遊戲開發', '心得'],
+    publishAt: '2025-10-02 23:53',
+  },
+  {
     title: '112年 跨考沒補習 500 小時清交資工備取心得',
     tags: ['心得', '研究所'],
     publishAt: '2023-03-10 04:16',
